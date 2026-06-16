1fbf55b28ec70dddc3eee15e8860d8132877c4f4
Yash Chavan <112128928+yash-s-chavan@users.noreply.github.com>
2026-06-16 10:52:17 -0400

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 2;
+        }
+        std::vector<int> dp(n+1, 0);
+        dp[n] = 1;
+        dp[n-1] = 1;
+        for(int i = n-2;i>=0;--i){
+            dp[i] = dp[i+1]+dp[i+2];
+        }
+        for (int element : dp) {
+            std::cout << element << " ";
+        }
+        return dp[0];
+
+    }
+};
